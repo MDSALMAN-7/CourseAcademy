35074c3d3a73f4ec50d3966237162dd21c5c4e4f
name <salmanmgr811201@gmail.com>
2025-09-18 15:40:19 +0530

    trigger redeploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-const stripePromise = loadStripe(
-  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
-); 
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY); 
  // publishable key it is for frontend key
 
 createRoot(document.getElementById("root")).render(
